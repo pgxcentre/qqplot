b4bcfba10c5135795012244d97463dd4e9094856
Marc-André Legault <legaultmarc@gmail.com>
2012-12-17 16:58:33 -0500

    Fully working version using R's static library to compute the qbeta function (for performance reasons).

--- create_qqplot.py
+++ create_qqplot.py
@@ -2,27 +2,55 @@
 # -*- coding: utf-8 -*-
 
 import os
+import os.path
+import ctypes
 import sys
 import argparse
 import re
 import math
+import gzip
 import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import scipy.stats as stats
 
 def main(args):
+    import matplotlib as mpl
+    if args.format != "X11" and mpl.get_backend() != "agg":
+        mpl.use("Agg")
+    import matplotlib.pyplot as plt
+    if args.format != "X11":
+        plt.ioff()
+
     # Read the file.
     observed = []
+    
+    # Configure the R Math library.
+    rlib = ctypes.cdll.LoadLibrary(args.rlib)
+    rlib.qbeta.restype = ctypes.c_double
+    qbeta = lambda p, a, b: rlib.qbeta(ctypes.c_double(p), 
+                                       ctypes.c_double(a),
+                                       ctypes.c_double(b),
+                                       0,
+                                       0)
+
     # TODO: Handle les fichiers zippes
-    with open(args.filename, 'rb') as f:
-        header = f.readline()
-        header = [re.sub(r"\r|\n", "", elem) for elem in header.split("\t")]
-        if not args.col in header:
-            raise Exception("Column '{0}' was not found.".format(args.col))
-        col_id = header.index(args.col)
-        for line in f:
-            observed.append(float(line.split("\t")[col_id]))
+    # Read three bytes of the file to see if zipped.
+    gzipped = False
+    f = open(args.filename, 'rb')
+    if f.read(3) == '\x1f\x8b\x08':
+        # File is not zipped.
+        gzipped = True
+    
+    if gzipped:
+        f.close()
+        f = gzip.open(args.filename, 'rb') 
+    
+    header = f.readline()
+    header = [re.sub(r"\r|\n", "", elem) for elem in header.split("\t")]
+    if not args.col in header:
+        raise Exception("Column '{0}' was not found.".format(args.col))
+    col_id = header.index(args.col)
+    for line in f:
+        observed.append(float(line.split("\t")[col_id]))
+
     observed = np.array(observed, dtype=float)
     observed = np.sort(observed)
     # Compute -log_10(p_value)
@@ -37,18 +65,25 @@ def main(args):
     axe.set_ylabel(args.ylabel)
     axe.set_title(args.title)
     # scipy.stats.beta.ppf(prb, a, b)
-    # c975 = []
-    # c025 = []
+    c975 = np.zeros(len(observed))
+    c025 = np.zeros(len(observed))
 
-    # for i in xrange(1, len(observed) + 1):
-    #     if i % 1000 == 0:
-    #         print "Long {}".format(i)
-    #     #FIXME
-    #     c975.append(stats.beta.ppf(0.975, i, len(observed) - i + 1))
-    #     c025.append(stats.beta.ppf(0.025, i, len(observed) - i + 1))
-    # axe.fill_between(expected, c025, c975)
-    plt.ioff()
-    plt.show()
+    for i in xrange(1, len(observed) + 1):
+        c975[i - 1] = qbeta(0.975, i, len(observed) - i + 1)
+        c025[i - 1] = qbeta(0.025, i, len(observed) - i + 1)
+    
+    c975 = -1 * np.log10(c975)
+    c025 = -1 * np.log10(c025)
+    axe.fill_between(expected, c025, c975, facecolor='#cccccc', edgecolor='#cccccc')
+
+    if args.format == "X11":
+        plt.show()
+    else:
+        if not re.search(r".+[.](pdf|ps|png)$", args.out):
+            args.out = "{}.{}".format(args.out, args.format)
+        plt.savefig(args.out, dpi=args.dpi, format=args.format)
+
+    f.close()
 
 
 if __name__ == '__main__':
@@ -91,7 +126,7 @@ if __name__ == '__main__':
                             help="Title for x axis",
                             dest="xlabel",
                             metavar="LABEL",
-                            default="Expected $-log_{10}(p)$"
+                            default="$-\\log_{10}(Expected)$"
                        )
 
     parser.add_argument(
@@ -100,8 +135,47 @@ if __name__ == '__main__':
                             help="Title for y axis",
                             dest="ylabel",
                             metavar="LABEL",
-                            default="Observed $-log_{10}(p)$"
+                            default="$-\\log_{10}(Observed)$"
                        )
+
+    parser.add_argument(
+                            "--format",
+                            type=str,
+                            help="Output file format",
+                            dest="format",
+                            default="png",
+                            choices=["png", "ps", "pdf", "X11"]
+                       )
+
+    parser.add_argument(
+                            "--out",
+                            type=str,
+                            help="Output file name",
+                            dest="out",
+                            default="qqplot"
+                       )
+
+    parser.add_argument(
+                            "--dpi",
+                            type=int,
+                            help="DPI of the exported image",
+                            dest="dpi",
+                            default=150
+                       )
+
+    parser.add_argument(
+                            "--Rlib",
+                            type=str,
+                            help=("R math stand-alone C library (.so) "
+                                  "to compute qbeta efficiently"),
+                            dest="rlib",
+                            metavar="RMath",
+                            default=os.path.join(
+                                os.path.dirname(__file__),
+                                "libRmath.so"
+                            )
+                        )
+
     args = parser.parse_args()
     try:
         main(args)
